865b2d888aa994c104e2f0bb67e2c16a0cf7e2e2
AaronNadar <aaronnadar07@gmail.com>
2026-08-20 01:01:54 +0530

    Change environment variable for password retrieval

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import random
 import smtplib
 
 my_email = os.environ.get("MY_EMAIL")
-password = os.environ.get("PASSWORD")
+password = os.environ.get("MY_PASSWORD")
 
 now = dt.datetime.now()
 today_month = now.month
